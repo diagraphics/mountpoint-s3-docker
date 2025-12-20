6392a9051c8923033d47eb4f72681fcf1a72f152
William West <wwest@diagraphics.com>
2025-12-20 17:55:43 -0500

    Remove obsolete js/ts material

--- src/request.ts
+++
@@ -1,8 +0,0 @@
-type RequestHead = Pick<Request, "headers" | "body" | "url" | "method">;
-type RequestContent = Pick<Request, "body" | "arrayBuffer">;
-
-interface SignableRequest extends RequestHead, RequestContent {
-  clone(): SignableRequest;
-}
-
-export type { SignableRequest };
